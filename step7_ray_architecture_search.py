d63459da6c5856a0028c2e85bc7768d99bed7d3d
JoelKt29 <joelkhayat@gmail.com>
2026-02-03 14:27:24 +0100

    Update step7_ray_architecture_search.py

--- step7_ray_architecture_search.py
+++ step7_ray_architecture_search.py
@@ -451,18 +451,20 @@ def run_ray_tune_search(
     tuner = tune.Tuner(
     tune.with_resources(
         train_model_ray,
-        resources={"cpu": 1, "gpu": gpus_per_trial}
+        resources={"cpu": 2, "gpu": 1}
     ),
     tune_config=tune.TuneConfig(
         scheduler=scheduler,
         search_alg=search_alg,
         num_samples=num_samples,
+        verbose=1,   # ✅ ici uniquement
     ),
     param_space=search_space,
     run_config=train.RunConfig(
-    name="sabr_tabpfn_search",
-    storage_path=os.path.abspath(output_dir),)
+        name="sabr_tabpfn_search",
+        storage_path=os.path.abspath(output_dir),
     )
+)
     
     results = tuner.fit()
     
